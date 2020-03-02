4fd6d4371d2a20917d591f26880b6e751b8b77f2
Thomas <thomasreuvers@upcmail.nl>
2020-03-02 21:48:55 +0100

    Initial commit

---
+++ BlackJack/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlackJack
+{
+    public class Card
+    {
+        public string FileLocation { get; set; }
+
+        public int Value { get; set; }
+
+        public bool IsAce { get; set; }
+    }
+}
